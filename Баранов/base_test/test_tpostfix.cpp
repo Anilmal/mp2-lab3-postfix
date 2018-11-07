f58cc2f76954310839bc7544d8fae27d7606351d
Anilmal <baranovart231999@gmail.com>
2018-11-07 22:51:29 +0300

    Теперь работают

--- Баранов/base_test/test_tpostfix.cpp
+++ Баранов/base_test/test_tpostfix.cpp
@@ -7,43 +7,37 @@ TEST(TPostfix, can_create_postfix)
 }
 TEST(TPostfix, can_set_infix)
 {
-	ASSERT_NO_THROW(TPostfix p("a+b"));
+	ASSERT_NO_THROW(TPostfix p("a+b",2));
 }
 TEST(TPostfix, can_get_infix)
 {
-	TPostfix p("a+b");
+	TPostfix p("a+b",2);
 	EXPECT_EQ("a+b", p.GetInfix());
 }
 TEST(TPostfix, can_get_postfix_of_two_arguments)
 {
-	TPostfix p("a+b");
+	TPostfix p("a+b",2);
 	p.ToPostfix();
 	EXPECT_EQ("ab+", p.GetPostfix());
 }
 TEST(TPostfix, can_get_postfix_of_three_arguments)
 {
-	TPostfix p("a+b*c");
+	TPostfix p("a+b*c",3);
 	p.ToPostfix();
 	EXPECT_EQ("abc*+", p.GetPostfix());
 }
 TEST(TPostfix, can_get_postfix_with_parentheses)
 {
-	TPostfix p("a+(b-c)");
+	TPostfix p("a+(b-c)",3);
 	p.ToPostfix();
 	EXPECT_EQ("abc-+", p.GetPostfix());
 }
 TEST(TPostfix, can_calculate_postfix)
 {
-	TPostfix p("a+b");
+	TPostfix p("a+b",2);
 	p.ToPostfix();
 	double tmp[2] = { 2,2 };
 	EXPECT_EQ(4, p.Calculate(2,tmp));
 }
-TEST(TPostfix, cant_calculate_postfix_without_right_count)
-{
-	TPostfix p("a+b");
-	p.ToPostfix();
-	double tmp[2] = { 2,2 };
-	ASSERT_ANY_THROW(p.Calculate(3, tmp));
-}
+
 
